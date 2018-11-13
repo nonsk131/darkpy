98c3c9b439ebb8edc8177f17681c9b194287037e
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-11-12 20:44:34 -0500

    updated

--- timeEvo.py
+++ timeEvo.py
@@ -19,6 +19,7 @@ def make_fig(pos_pa, i):
     ax.set_xlabel('x [kpc]')
     ax.set_ylabel('z [kpc]')
     fig.savefig('/mnt/home/npanithanpaisal/darkpy/figs/xz_snap{}.png'.format(i), dpi=300)
+    print('xz_snap{}.png has been created'.format(i))
 
 # read in stars (no dark matter) at z = 0, snapshot 600
 part_600 = gizmo.io.Read.read_snapshots(['star'], 'index', 600, assign_principal_axes=True,
@@ -29,23 +30,19 @@ st = np.loadtxt('one-stream-ids.txt', dtype=int)
 pos_pa600 = ut.coordinate.get_coordinates_rotated(part_600['star']['host.distance'][st],part_600.principal_axes_vectors)
 make_fig(pos_pa600, 600)
 
-for i in range(400, 600, 1):
-    try:
-        print("processing snapshpt {}".format(i))
-        # read in stars at snapshot i
-        part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_principal_axes=True,
-                                         assign_orbit=True,
-                                         simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+for i in range(598, 600, 1):
+    # read in stars at snapshot i
+    part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_principal_axes=True,
+                                     assign_orbit=True,
+                                     simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
 
-        # read star index pointer
-        gizmo.track.IndexPointer.io_index_pointer(part_i)
-        st_i = part_i.index_pointers[st]
+    # read star index pointer
+    gizmo.track.IndexPointer.io_index_pointer(part_i)
+    st_i = part_i.index_pointers[st]
 
-        # check that all the indices are not null
-        if np.isnan(indices_at_i).sum() > 0:
-            print(i)
+    # check that all the indices are not null
+    if np.isnan(indices_at_i).sum() > 0:
+        print(i)
 
-        pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_i.principal_axes_vectors)
-        make_fig(pos_pa_i, i)
-    except:
-        continue
+    pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_i.principal_axes_vectors)
+    make_fig(pos_pa_i, i)
