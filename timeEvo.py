5c86303a926ec8fe5b8a0814e94f1082186f4d7c
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-11-12 19:04:57 -0500

    updated

--- timeEvo.py
+++ timeEvo.py
@@ -1,1 +1,11 @@
 import numpy as np
+import gizmo_analysis as gizmo
+import wutilities as ut
+import rockstar_analysis
+
+# read in stars (no dark matter) at z = 0
+part600 = gizmo.io.Read.read_snapshots(['star'], 'redshift', 0, assign_principal_axes=True,
+                                 assign_orbit=True,
+                                 simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+
+print('end of script')
