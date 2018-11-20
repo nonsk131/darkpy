beda05e90adf38151d16b0cfe90d6e8318cb24c2
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-11-19 23:04:51 -0500

    not using principal axes

--- timeEvo.py
+++ timeEvo.py
@@ -1,7 +1,7 @@
 import numpy as np
 import gizmo_analysis as gizmo
 import wutilities as ut
-import rockstar_analysis
+import rockstar_analysis as rockstar
 import matplotlib.pyplot as plt
 import os
 
@@ -13,14 +13,18 @@ if not os.path.exists(directory):
 def make_fig(pos_pa, i):
     fig = plt.figure(figsize=(10,8))
     ax = fig.add_subplot(1,1,1)
-    ax.scatter(pos_pa[:,0], pos_pa[:,2], s=4, c='black', alpha=0.3)
+    ax.scatter(pos_pa[:,0], pos_pa[:,1], s=4, c='black', alpha=0.3)
     ax.set_xlim((-80, 80))
     ax.set_ylim((-80, 80))
     ax.set_xlabel('x [kpc]')
     ax.set_ylabel('z [kpc]')
-    fig.savefig('/mnt/home/npanithanpaisal/darkpy/figs/xz_snap{}.png'.format(i), dpi=300)
-    print('xz_snap{}.png has been created'.format(i))
+    fig.savefig('/mnt/home/npanithanpaisal/darkpy/figs/xy_snap{}.png'.format(i), dpi=300)
+    print('xy_snap{}.png has been created'.format(i))
     plt.close(fig)
+
+# read in merger tree
+halt = rockstar.io.IO.read_tree(simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+hal600 = rockstar.io.IO.get_catalog_from_tree(halt, 600)
 
 # read in stars (no dark matter) at z = 0, snapshot 600
 part_600 = gizmo.io.Read.read_snapshots(['star'], 'index', 600, assign_principal_axes=True,
@@ -32,7 +36,7 @@ st = np.loadtxt('one-stream-ids.txt', dtype=int)
 pos_pa600 = part_600['star']['host.distance'][st]
 make_fig(pos_pa600, 600)
 
-for i in range(590, 600, 1):
+for i in range(580, 600, 1):
     try:
         # read in stars at snapshot i
         part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_principal_axes=True,
