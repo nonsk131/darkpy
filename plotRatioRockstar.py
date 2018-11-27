0eccc2bba79264de49fd6ec2b25e8bb29bb28fb8
Nondh Panithanpaisal <nondh@modv-ve501-0460.apn.wlan.upenn.edu>
2018-11-27 18:40:16 -0500

    add file

---
+++ plotRatioRockstar.py
@@ -0,0 +1,17 @@
+import numpy as np
+import wutilities as ut
+import rockstar_analysis as rockstar
+import matplotlib.pyplot as plt
+
+halt = rockstar.io.IO.read_tree(simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+n_tree = np.zeros(601)
+n_cata = np.zeros(601)
+n_tree[0] = 1
+n_cata[0] = 1
+for i in range(1,601,1):
+    hal_i_tree = rockstar.io.IO.get_catalog_from_tree(halt, i)
+    hal_i_cata = rockstar.io.IO.read_catalogs('index', 600, simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+    n_tree[i] = len(hal_i_tree['host.distance'])
+    n_cata[i] = len(hal_i_cata['host.distance'])
+
+np.savetxt('/mnt/home/npanithanpaisal/darkpy/ratio.txt', np.column_stack((n_tree, n_cata, n_tree/n_cata)), header='halos from tree, halos from read_catalogs, ratio')
