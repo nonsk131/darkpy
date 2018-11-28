1db8967e2087d88be030537c433214f7978e80ed
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-11-28 18:59:18 -0500

    fixed

--- plotRatioRockstar.py
+++ plotRatioRockstar.py
@@ -10,7 +10,7 @@ n_tree[0] = 1
 n_cata[0] = 1
 for i in range(1,601,1):
     hal_i_tree = rockstar.io.IO.get_catalog_from_tree(halt, i)
-    hal_i_cata = rockstar.io.IO.read_catalogs('index', 600, simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+    hal_i_cata = rockstar.io.IO.read_catalogs('index', i, simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
     n_tree[i] = len(hal_i_tree['host.distance'])
     n_cata[i] = len(hal_i_cata['host.distance'])
 
