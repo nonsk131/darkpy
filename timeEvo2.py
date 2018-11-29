c3a5eab1b2964738bb4133c6448c309ee544e214
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-11-29 16:06:09 -0500

    update

--- timeEvo2.py
+++ timeEvo2.py
@@ -64,63 +64,63 @@ part_600 = gizmo.io.Read.read_snapshots(['star'], 'index', 600, assign_host_prin
 st = np.loadtxt('one-stream-ids.txt', dtype=int)
 
 for i in range(600, 601, 1):
-    try:
+    #try:
     # read in stars at snapshot i
-        part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_host_principal_axes=True,
-                                         assign_host_orbits=True,
-                                         simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
-        hal_i_ind = np.where(halt['snapshot'] == i)[0]
+    part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_host_principal_axes=True,
+                                     assign_host_orbits=True,
+                                     simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+    hal_i_ind = np.where(halt['snapshot'] == i)[0]
 
-        if i < 600:
-            # read star index pointer
-            gizmo.track.ParticleIndexPointer.io_pointers(part_i, directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/track/')
-            st_i = part_i.index_pointers[st]
-        else:
-            st_i = st
-        pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_600.principal_axes_vectors)
-        pos_pa_hal_i = ut.coordinate.get_coordinates_rotated(halt['host.distance'][hal_i_ind],part_600.principal_axes_vectors)
-        count, id = compute_dist(pos_pa_i, pos_pa_hal_i)
-        jj = hal_i_ind[id]
-        for j in jj:
-            if j not in indices[i]:
-                indices[i].append(j)
-            if j not in all_i:
-                mass.append(halt['mass'][j])
-                all_i.append(j)
+    if i < 600:
+        # read star index pointer
+        gizmo.track.ParticleIndexPointer.io_pointers(part_i, directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/track/')
+        st_i = part_i.index_pointers[st]
+    else:
+        st_i = st
+    pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_600.principal_axes_vectors)
+    pos_pa_hal_i = ut.coordinate.get_coordinates_rotated(halt['host.distance'][hal_i_ind],part_600.principal_axes_vectors)
+    count, id = compute_dist(pos_pa_i, pos_pa_hal_i)
+    jj = hal_i_ind[id]
+    for j in jj:
+        if j not in indices[i]:
+            indices[i].append(j)
+        if j not in all_i:
+            mass.append(halt['mass'][j])
+            all_i.append(j)
 
-        c = np.zeros(2)
-        c[1] = count
-        np.savetxt('/mnt/home/npanithanpaisal/darkpy/t.txt', c)
+    c = np.zeros(2)
+    c[1] = count
+    np.savetxt('/mnt/home/npanithanpaisal/darkpy/t.txt', c)
 
-        # track the halos in N previous and N after snapshots
-        N = 3
-        n = 1
-        prog_index = jj
-        while n <= N:
-            # going backward
-            prog_index = halt['progenitor.main.index'][prog_index]
-            prog_index = prog_index[np.where(prog_index > 0)]
-            for k in prog_index:
-                if k not in indices[i-n]:
-                    indices[i-n].append(k)
-            n += 1
+    # track the halos in N previous and N after snapshots
+    N = 3
+    n = 1
+    prog_index = jj
+    while n <= N:
+        # going backward
+        prog_index = halt['progenitor.main.index'][prog_index]
+        prog_index = prog_index[np.where(prog_index > 0)]
+        for k in prog_index:
+            if k not in indices[i-n]:
+                indices[i-n].append(k)
+        n += 1
 
-        if i + N > 600:
-            N = 600 - i
-        n = 1
-        des_index = jj
-        while n <= N:
-            # going forward
-            des_index = halt['descendant.index'][des_index]
-            des_index = des_index[np.where(des_index > 0)]
-            for k in des_index:
-                if k not in indices[i+n]:
-                    indices[i+n].append(k)
-            n += 1
+    if i + N > 600:
+        N = 600 - i
+    n = 1
+    des_index = jj
+    while n <= N:
+        # going forward
+        des_index = halt['descendant.index'][des_index]
+        des_index = des_index[np.where(des_index > 0)]
+        for k in des_index:
+            if k not in indices[i+n]:
+                indices[i+n].append(k)
+        n += 1
 
 
-            #make_fig(pos_pa_i, i, pos_pa_hal_i[interacting_hal_id], count)
+        #make_fig(pos_pa_i, i, pos_pa_hal_i[interacting_hal_id], count)
 
-    except:
-        continue
+    #except:
+    #    continue
 #np.savetxt('/mnt/home/npanithanpaisal/darkpy/count.txt', c, header='number of interacting halos as a function of index, which is represented by row')
