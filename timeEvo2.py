af539bc9e4c008a4989f96cb647c7ef8f46fc5f6
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-11-29 14:45:47 -0500

    add file

---
+++ timeEvo2.py
@@ -0,0 +1,89 @@
+import numpy as np
+import gizmo_analysis as gizmo
+import utilities as ut
+import rockstar_analysis as rockstar
+import matplotlib.pyplot as plt
+import os
+
+file_path = "/mnt/home/npanithanpaisal/darkpy/figs/"
+directory = os.path.dirname(file_path)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+def make_fig(pos_pa, i, pos_pa_hal, count):
+    fig = plt.figure(figsize=(10,8))
+    ax = fig.add_subplot(1,1,1)
+    ax.scatter(pos_pa[:,0], pos_pa[:,2], s=4, c='black', alpha=0.3)
+    ax.scatter(pos_pa_hal[:,0], pos_pa_hal[:,2], s=50, c='red')
+    ax.set_xlim((-80, 80))
+    ax.set_ylim((-80, 80))
+    ax.set_xlabel('x [kpc]')
+    ax.set_ylabel('z [kpc]')
+    ax.set_title('snapshot: {}   count: {}'.format(i, count))
+    fig.savefig('/mnt/home/npanithanpaisal/darkpy/figs/xz_snap{}.png'.format(i), dpi=300)
+    print('xz_snap{}.png has been created'.format(i))
+    plt.close(fig)
+
+# pos = part['star']['host.distance'][st]
+# pos_halo = hal['host.distance']
+def compute_dist(pos, pos_hal, hal, threshold = 2):
+
+    count = 0
+    halo_ind = []
+    for i in range(len(pos_hal)):
+        # throw away those that are clearly too far away
+        if np.absolute(pos_hal[i,0]) > 80 or np.absolute(pos_hal[i,1]) > 80 or np.absolute(pos_hal[i,2]) > 80:
+            continue
+
+        else:
+            d_hal = np.tile(pos_hal[i], len(pos)).reshape((len(pos),3))
+            d_star = pos
+            d_star_hal = np.sqrt(((d_hal - d_star)**2).sum(axis=1))
+            if np.min(d_star_hal) < threshold:
+                count += 1
+                halo_ind.append(i)
+
+    return count, np.array(halo_ind)
+
+
+#-------------------------------------------------------------------------------
+# store indices of halos that we have to draw in each snapshot
+indices = {k: [] for k in range(1,601,1)}
+
+# read in merger tree using the new merger tree
+halt = rockstar.io.IO.read_tree(simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/',
+    rockstar_directory='halo/rockstar_dm_new')
+
+# read in stars (no dark matter) at z = 0, snapshot 600 just to get the principal axis
+part_600 = gizmo.io.Read.read_snapshots(['star'], 'index', 600, assign_principal_axes=True,
+                                 assign_orbit=True,
+                                 simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+# read in indices of stars in the stream
+st = np.loadtxt('one-stream-ids.txt', dtype=int)
+
+for i in range(600, 601, 1):
+    try:
+        # read in stars at snapshot i
+        part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_principal_axes=True,
+                                         assign_orbit=True,
+                                         simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+        hal_i_ind = np.where(halt['snapshot'] == i)[0]
+
+        # read star index pointer
+        gizmo.track.ParticleIndexPointer.io_pointers(part_i, directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/track/')
+        st_i = part_i.index_pointers[st]
+
+        pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_600.principal_axes_vectors)
+        pos_pa_hal_i = ut.coordinate.get_coordinates_rotated(halt['host.distance'][hal_i_ind],part_600.principal_axes_vectors)
+        count, id = compute_dist(pos_pa_i, pos_pa_hal_i)
+        for j in id:
+            indices[i].append(hal_i_ind[j])
+
+        print(count)
+        print(indices[600])
+
+        #make_fig(pos_pa_i, i, pos_pa_hal_i[interacting_hal_id], count)
+
+    except:
+        continue
+np.savetxt('/mnt/home/npanithanpaisal/darkpy/count.txt', c, header='number of interacting halos as a function of index, which is represented by row')
