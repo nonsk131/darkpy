2cd1bc85d9fc337aa89b3969db6be642ecd48106
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-11-28 21:16:17 -0500

    update

--- plotRatioRockstar.py
+++ plotRatioRockstar.py
@@ -10,8 +10,7 @@ for i in range(250,601,1):
     print('doing index {}'.format(i))
     hal_i_tree = rockstar.io.IO.get_catalog_from_tree(halt, i)
     hal_i_cata = rockstar.io.IO.read_catalogs('index', i, simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
-    #hal_i_cata += hal_i_cata['am.phantom'].sum()
-    n_tree[i] = len(hal_i_tree['host.distance']) - hal_i_tree['am.phantom'].sum()
+    n_tree[i] = len(hal_i_tree['host.distance'])
     n_cata[i] = len(hal_i_cata['host.distance'])
 
 np.savetxt('/mnt/home/npanithanpaisal/darkpy/ratio.txt', np.column_stack((n_tree.astype(int), n_cata.astype(int), n_tree/n_cata)), header='halos from tree, halos from read_catalogs, ratio')
