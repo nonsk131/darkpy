6daa60fee8fa93d6298a2529ec27f10a0a6923df
Nondh Panithanpaisal <nondh@Nondhs-MacBook-Pro.local>
2018-12-13 12:49:33 -0500

    update

--- getMass.py
+++ getMass.py
@@ -1,11 +1,11 @@
 import numpy as np
-import gizmo_analysis as gizmo
-import utilities as ut
-import rockstar_analysis as rockstar
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
+import gizmo_analysis as gizmo
+import utilities as ut
+import rockstar_analysis as rockstar
 
 # read in merger tree using the new merger tree
 halt = rockstar.io.IO.read_tree(simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/',
