076110d6e086d92455dad39d2b377ebaf24d9f0b
Nondh Panithanpaisal <nondh@modv-ve502-0568.apn.wlan.upenn.edu>
2018-12-13 15:00:43 -0500

    update

--- timeEvo2.py
+++ timeEvo2.py
@@ -1,11 +1,11 @@
 import numpy as np
-import gizmo_analysis as gizmo
-import utilities as ut
-import rockstar_analysis as rockstar
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
+import gizmo_analysis as gizmo
+import utilities as ut
+import rockstar_analysis as rockstar
 import os
 
 file_path = "/mnt/home/npanithanpaisal/darkpy/figs2/"
@@ -51,12 +51,50 @@ def compute_dist(pos, pos_hal, threshold = 2):
 
     return count, np.array(halo_ind)
 
+def compute_dist_vel(pos, vel, pos_hal, vel_hal, threshold = 2, N = 10):
+        count = 0
+        halo_ind = []
+        mindist = []
+        relative = []
+        for i in range(len(pos_hal)):
+            # throw away those that are clearly too far away
+            if np.absolute(pos_hal[i,0]) > 80 or np.absolute(pos_hal[i,1]) > 80 or np.absolute(pos_hal[i,2]) > 80:
+                continue
+
+            else:
+                # compute distance for all stars in the stream
+                d_hal = np.tile(pos_hal[i], len(pos)).reshape((len(pos),3))
+                d_star = pos
+                d_star_hal = np.sqrt(((d_hal - d_star)**2).sum(axis=1))
+                if np.min(d_star_hal) < threshold:
+                    count += 1
+                    halo_ind.append(i)
+                    mindist.append(np.min(d_star_hal))
+
+                    d_and_v = np.column_stack((d_star_hal, vel))
+                    # sort by first column
+                    d_and_v = d_and_v[d_and_v[:,0].argsort()]
+                    v_star_mean = np.mean(d_and_v[:N,1:], axis=0)
+
+                    v_hal = vel_hal[i]
+                    v_rel = np.sqrt(((v_hal - v_star_mean)**2).sum())
+                    relative.append(v_rel)
+
+        halo_ind = np.array(halo_ind)
+        mindist = np.array(mindist)
+        relative = np.array(relative)
+        return count, halo_ind, mindist, relative
+
 
 #-------------------------------------------------------------------------------
-# store indices of halos that we have to draw in each snapshot
-indices = {k: [] for k in range(1,601,1)}
+# store indices of interacting halos in each snapshot
+interacting = {k: [] for k in range(1,601,1)}
+# interpolate
+interpolate = {k: [] for k in range(1,601,1)}
 masses = []
 all_i = []
+all_d = []
+all_v = []
 c = np.zeros(601)
 
 # read in merger tree using the new merger tree
@@ -70,64 +108,67 @@ part_600 = gizmo.io.Read.read_snapshots(['star'], 'index', 600, assign_host_prin
 # read in indices of stars in the stream
 st = np.loadtxt('one-stream-ids.txt', dtype=int)
 
-start = 250
+start = 598
 for i in range(start, 601, 1):
-    try:
+    print('analyzing snapshot {}'.format(i))
 
-        # read in stars at snapshot i
-        part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_host_principal_axes=True,
-                                         assign_host_orbits=True,
-                                         simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
-        hal_i_ind = np.where(halt['snapshot'] == i)[0]
+    # read in stars at snapshot i
+    part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_host_principal_axes=True,
+                                     assign_host_orbits=True,
+                                     simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+    hal_i_ind = np.where(halt['snapshot'] == i)[0]
 
-        if i < 600:
-            # read star index pointer
-            gizmo.track.ParticleIndexPointer.io_pointers(part_i, directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/track/')
-            st_i = part_i.index_pointers[st]
-        else:
-            st_i = st
-        pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_600.host_rotation_tensors[0])
-        pos_pa_hal_i = ut.coordinate.get_coordinates_rotated(halt['host.distance'][hal_i_ind],part_600.host_rotation_tensors[0])
-        count, id = compute_dist(pos_pa_i, pos_pa_hal_i)
-        c[i] = count
-        jj = hal_i_ind[id]
-        for j in jj:
-            if j not in indices[i]:
-                indices[i].append(j)
-            if j not in all_i:
-                masses.append(halt['mass'][j])
-                all_i.append(j)
+    if i < 600:
+        # read star index pointer
+        gizmo.track.ParticleIndexPointer.io_pointers(part_i, directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/track/')
+        st_i = part_i.index_pointers[st]
+    else:
+        st_i = st
+    pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_600.host_rotation_tensors[0])
+    pos_pa_hal_i = ut.coordinate.get_coordinates_rotated(halt['host.distance'][hal_i_ind],part_600.host_rotation_tensors[0])
+    vel_i = part_i['star']['host.velocity'][st_i]
+    vel_hal_i = halt['host.velocity'][hal_i_ind]
+    count, id, d, v = compute_dist_vel(pos_pa_i, vel_i, pos_pa_hal_i, vel_hal_i)
+    all_d.append(d)
+    all_v.append(v)
+    c[i] = count
+    # where in the big merger tree
+    jj = hal_i_ind[id]
+    for j in jj:
+        if j not in interacting[i]:
+            interacting[i].append(j)
+        if j not in all_i:
+            masses.append(halt['mass'][j])
+            all_i.append(j)
 
-        # track the halos in N previous and N after snapshots
-        N = 4
-        n = 1
-        prog_index = jj
-        while n <= N:
-            # going backward
-            prog_index = halt['progenitor.main.index'][prog_index]
-            prog_index = prog_index[np.where(prog_index > 0)]
-            for k in prog_index:
-                if k not in indices[i-n]:
-                    indices[i-n].append(k)
-            n += 1
+    # track the halos in N previous and N after snapshots
+    N = 3
+    n = 1
+    prog_index = jj
+    while n <= N:
+        # going backward
+        prog_index = halt['progenitor.main.index'][prog_index]
+        prog_index = prog_index[np.where(prog_index > 0)]
+        for k in prog_index:
+            if k not in interpolate[i-n]:
+                interpolate[i-n].append(k)
+        n += 1
 
-        if i + N > 600:
-            N = 600 - i
-        n = 1
-        des_index = jj
-        while n <= N:
-            # going forward
-            des_index = halt['descendant.index'][des_index]
-            des_index = des_index[np.where(des_index > 0)]
-            for k in des_index:
-                if k not in indices[i+n]:
-                    indices[i+n].append(k)
-            n += 1
+    if i + N > 600:
+        N = 600 - i
+    n = 1
+    des_index = jj
+    while n <= N:
+        # going forward
+        des_index = halt['descendant.index'][des_index]
+        des_index = des_index[np.where(des_index > 0)]
+        for k in des_index:
+            if k not in interpolate[i+n]:
+                interpolate[i+n].append(k)
+        n += 1
 
 
-            #make_fig(pos_pa_i, i, pos_pa_hal_i[interacting_hal_id], count)
-    except:
-        continue
+        #make_fig(pos_pa_i, i, pos_pa_hal_i[interacting_hal_id], count)
 
 # save id and masses
 id_mass = np.column_stack((np.array(all_i), np.array(masses)))
@@ -137,22 +178,19 @@ cc = np.column_stack((np.array(range(0,601,1)), c))
 np.savetxt('/mnt/home/npanithanpaisal/darkpy/halos/count.txt', cc)
 
 for i in range(start,601,1):
-    try:
-        name = 'interacting_snap{}.txt'.format(i)
-        path = os.path.join('/mnt/home/npanithanpaisal/darkpy/halos/', name)
-        np.savetxt(path, np.array(indices[i]))
+    name = 'interacting_snap{}.txt'.format(i)
+    path = os.path.join('/mnt/home/npanithanpaisal/darkpy/halos/', name)
+    np.savetxt(path, np.array(indices[i]))
 
-        part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_host_principal_axes=True,
-                                         assign_host_orbits=True,
-                                         simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
-        if i < 600:
-            # read star index pointer
-            gizmo.track.ParticleIndexPointer.io_pointers(part_i, directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/track/')
-            st_i = part_i.index_pointers[st]
-        else:
-            st_i = st
-        pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_600.host_rotation_tensors[0])
-        pos_pa_hal_i = ut.coordinate.get_coordinates_rotated(halt['host.distance'][indices[i]],part_600.host_rotation_tensors[0])
-        make_fig(pos_pa_i, i, pos_pa_hal_i, c[i])
-    except:
-        continue
+    part_i = gizmo.io.Read.read_snapshots(['star'], 'index', i, assign_host_principal_axes=True,
+                                     assign_host_orbits=True,
+                                     simulation_directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100')
+    if i < 600:
+        # read star index pointer
+        gizmo.track.ParticleIndexPointer.io_pointers(part_i, directory='/mnt/ceph/users/firesims/fire2/metaldiff/m12i_res7100/track/')
+        st_i = part_i.index_pointers[st]
+    else:
+        st_i = st
+    pos_pa_i = ut.coordinate.get_coordinates_rotated(part_i['star']['host.distance'][st_i],part_600.host_rotation_tensors[0])
+    pos_pa_hal_i = ut.coordinate.get_coordinates_rotated(halt['host.distance'][indices[i]],part_600.host_rotation_tensors[0])
+    make_fig(pos_pa_i, i, pos_pa_hal_i, c[i])
